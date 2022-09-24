f9b928aedbd5e699314fea8d6ef71b710a22b209
jarol1590 <jarol1590@gmail.com>
2022-09-24 00:04:58 -0500

    Saca error en el archivo application porque no encuentra el RepositoryMixin, se trata de instalar pero no funciona

---
+++ src/config/general-config.ts
@@ -0,0 +1,8 @@
+
+
+
+export namespace GeneralConfig {
+  export const url_ms_seguridad = "http://localhost:3001";
+  export const metodo_validar_jwt = "validate-token";
+  export const adminRolId = "632e5522a08b4c6af8806057";
+}
---
+++ src/strategies/admin-authorization-strategies.ts
@@ -0,0 +1,34 @@
+import {AuthenticationStrategy} from '@loopback/authentication';
+import {BindingScope, injectable} from '@loopback/core';
+import {HttpErrors, Request} from '@loopback/rest';
+import {UserProfile} from '@loopback/security';
+import fetch from 'node-fetch';
+import parseBearerToken from 'parse-bearer-token';
+import {GeneralConfig} from '../config/general-config';
+
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class AdminAuthorizationStrategy implements AuthenticationStrategy {
+  name = "admin";
+
+
+  async authenticate(request: Request): Promise<UserProfile | undefined> {
+    const token = parseBearerToken(request);
+    console.log(`Token de la request: ${token}`);
+    if (token) {
+      const esValido = await fetch(`${GeneralConfig.url_ms_seguridad}/${GeneralConfig.metodo_validar_jwt}/${token}`)
+      let rolId = await esValido.text();
+      if (rolId == GeneralConfig.adminRolId) {
+        // validar el id
+        let perfil: UserProfile = Object.assign({
+          admin: 'OK',
+        });
+        return perfil;
+      } else {
+        throw new HttpErrors[401]("Solicitud rechazada porque el token enviado no es válido.");
+      }
+    } else {
+      throw new HttpErrors[401]("Solicitud rechazada porque no tiene un token.");
+    }
+  }
+}
