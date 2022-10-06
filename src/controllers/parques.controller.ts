af48e6270ec10274ef3f6a5a136cbd6bcf182d29
jarol1590 <jarol1590@gmail.com>
2022-10-06 01:09:32 -0500

    se pone estrategia de admin en controlador de parques

--- src/controllers/parques.controller.ts
+++ src/controllers/parques.controller.ts
@@ -1,30 +1,25 @@
+import {authenticate} from '@loopback/authentication';
 import {
   Count,
   CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  del, get,
+  getModelSchemaRef, param, patch, post, put, requestBody,
+  response
 } from '@loopback/rest';
 import {Parque} from '../models';
 import {ParqueRepository} from '../repositories';
-
+@authenticate("admin")
 export class ParquesController {
   constructor(
     @repository(ParqueRepository)
-    public parqueRepository : ParqueRepository,
-  ) {}
+    public parqueRepository: ParqueRepository,
+  ) { }
 
   @post('/parques')
   @response(200, {
